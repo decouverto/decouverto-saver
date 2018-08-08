3cb4fec0836ac7974fb074fd62fd52e06b4f375c
Cédric JUNG <cedced19@gmail.com>
2018-08-08 21:21:50 +0200

    Add possibility to download all repos

--- src/js/index.js
+++ src/js/index.js
@@ -9,15 +9,14 @@ const githubRepositories = require('github-repositories');
 const DownloadManager = remote.require('electron-download-manager');
 
 const mainFolder = path.join(app.getPath('desktop'), 'Sauvegarde Decouverto');
-// app.getPath('desktop')
 
 angular.module('UI', ['ngNotie'])
     .controller('UICtrl', ($scope, notie, $http) => {
-       
+
         $http({
             method: 'GET',
             url: 'https://decouverto.fr/walks/index.json'
-        }).then(res  => {
+        }).then(res => {
             $scope.walks = res.data;
         }, () => {
             notie.alert(3, 'Une erreur a eu lieu dans la synchronisation avec le site internet.');
@@ -32,7 +31,7 @@ angular.module('UI', ['ngNotie'])
             fs.mkdirp(path.join(mainFolder, 'logiciels', 'tmp'), function (err) {
                 if (err) return notie.alert(3, 'Impossible de créer le dossier');
                 DownloadManager.download({
-                    url: 'https://codeload.github.com/' + repo.full_name + '/zip/' + repo.default_branch,             
+                    url: 'https://codeload.github.com/' + repo.full_name + '/zip/' + repo.default_branch,
                     path: path.join('logiciels', 'tmp')
                 }, err => {
                     if (err) return notie.alert(3, 'Une erreur a eu lieu lors du téléchargement.');
@@ -41,10 +40,44 @@ angular.module('UI', ['ngNotie'])
                         if (err) return notie.alert(3, 'Une erreur a eu lieu lors de la copie.');
                         fs.remove(path.join(mainFolder, 'logiciels', 'tmp'), (err) => {
                             if (err) return notie.alert(3, 'Une erreur a eu lieu lors du nettoyage.');
+                            repo.downloaded = true;
+                            $scope.$apply();
                             notie.alert(1, 'Téléchargement réussi.');
                         });
                     });
                 });
             });
         };
-})
+
+        $scope.downloadAllRepos = function () {
+            let links = [];
+            let filenames = [];
+            $scope.repos.forEach((repo) => {
+                links.push('https://codeload.github.com/' + repo.full_name + '/zip/' + repo.default_branch);
+                filenames.push(repo.name + '-' + repo.default_branch + '.zip');
+            });
+            DownloadManager.bulkDownload({
+                urls: links,
+                path: path.join('logiciels', 'tmp')
+            }, function (err, finished) {
+                if (err) return notie.alert(3, 'Une erreur a eu lieu lors du téléchargement.');
+                try {
+                    filenames.forEach(el => {
+                        fs.copySync(path.join(mainFolder, 'logiciels', 'tmp', el), path.join(mainFolder, 'logiciels', el));
+                    });
+                    fs.removeSync(path.join(mainFolder, 'logiciels', 'tmp'));
+                    if (finished.length == links.length) {
+                        notie.alert(1, 'Téléchargement réussi.');
+                        $scope.repos.forEach(el => {
+                            el.downloaded = true;
+                        });
+                        $scope.$apply();
+                    } else {
+                        notie.alert(2, 'Des logiciels n\'ont pas été téléchargés.');
+                    }
+                } catch (e) {
+                    notie.alert(3, 'Une erreur a eu lieu lors de la copie des logiciels.');
+                }
+            });
+        }
+    });
